9f73c8e7edb84b5ec464fb4f6b25df62c3640e9a
Einarr07 <aidenperse@gmail.com>
2024-10-21 23:16:50 -0500

    Creación del micro servicio para compra de inmuebles

---
+++ src/main/java/com/einarr/spring_boot_microservise_2_compra/SpringBootMicroservise2CompraApplication.java
@@ -0,0 +1,13 @@
+package com.einarr.spring_boot_microservise_2_compra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservise2CompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservise2CompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/einarr/spring_boot_microservise_2_compra/SpringBootMicroservise2CompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einarr.spring_boot_microservise_2_compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservise2CompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
